d6f2303525cf413bb04c550bbc194850650e348b
Shivam30Mishra <theshivammishra10@gmail.com>
2026-04-03 12:25:27 +0530

    Network security system is built

--- setup.py
+++ setup.py
@@ -1,36 +1,39 @@
 '''
-The setup.py file is an essential part of packaging and distributing python projects. It is used by setup tools(or distutils in older Python Versions) to define the configuration of your project such as it's metadata, dependencies and more
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project, such as its metadata, dependencies, and more
 '''
 
-from setuptools import find_packages, setup
-from typing     import List
+from setuptools import find_packages,setup
+from typing import List
 
 def get_requirements()->List[str]:
-  """
-  This function will return list of requirements
-  """
-  requirement_lst:List[str]=[]
-  try:
-    with open('requirements.txt','r') as file:
-      # Read lines from the files
-      lines = file.readlines()
-      # process each lines
-      for line in lines:
-        requirement = line.strip()
-        # ignore empty line and -e
-        if requirement and requirement!='-e .':
-          requirement_lst.append(requirement)
-  except FileNotFoundError:
-    print("requirements.txt file not found")
-  return requirement_lst
+    """
+    Thiss function will return list of requirements
+    
+    """
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            #Read lines from the file
+            lines=file.readlines()
+            ## Process each line
+            for line in lines:
+                requirement=line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
 
-print(get_requirements())
+    return requirement_lst
 
 setup(
-  name="NetworkSecurity",
-  version="0.0.1",
-  author="Shivam Mishra",
-  author_email="theshivammishra10@gmail.com",
-  packages=find_packages(),
-  install_requires=get_requirements()
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Krish Naik",
+    author_email="krishnaik06@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
 )
